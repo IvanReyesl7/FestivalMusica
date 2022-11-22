a05a90e18ecec2357e75d10c9f502d95b7d4518a
Ivan Reyes <Ivan_Reyesl5@outlook.com>
2022-11-22 09:04:12 -0600

    normalize and globals

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
-const { src, dest } = require("gulp");
+const { src, dest, watch } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const plumber = require("gulp-plumber");
 
 function css(done){
     
-    src('src/scss/app.scss')//identificar archivo sass
+    src('src/scss/**/*.scss')//identificar archivo sass
+        .pipe(plumber())
         .pipe(sass())//compilador
         .pipe(dest("build/css"));//almacenar en disco duro
     
@@ -13,4 +15,12 @@ function css(done){
     done();//callback
 }
 
+function dev(done){
+
+    watch('src/scss/**/*.scss',css);
+
+    done();
+}
+
 exports.css = css;
+exports.dev = dev;
