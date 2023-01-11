f8687214a419eea928e8bdc6d808152bcd0728ce
Ivan <ivan_reyesl5@outlook.com>
2023-01-10 21:35:16 -0600

    fijar body

--- src/js/app.js
+++ src/js/app.js
@@ -33,11 +33,30 @@ function mostrarImagen(id){
         <source srcset="build/img/grande/${id}.avif" type="image/avif"> <source srcset="build/img/grande/${id}.webp" type="image/webp"> <img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg" alt="vocalista">
         `;
 
+        //crear el overlay
         const overlay = document.createElement('div');
         overlay.appendChild(imagen);
         overlay.classList.add('overlay');
 
+        //Boton para cerrar el modal 
+
+        const cerrarModal = document.createElement('p');
+        cerrarModal.textContent = "x";
+        cerrarModal.classList.add('btn-cerrar');
+        cerrarModal.onclick = function () {
+
+            const body = document.querySelector("body");
+            body.classList.remove('fijar-body');
+            overlay.remove();
+        }
+        overlay.appendChild(cerrarModal);
+
+
+        //añadir al html 
         const body = document.querySelector("body");
         body.appendChild(overlay);
-    
+        body.classList.add('fijar-body');
+
+
+        
 }
